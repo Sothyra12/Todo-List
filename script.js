6be7a5982b5cb33ebd0f5b72abfb385309a79fda
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-15 21:00:14 -0400

    added antoher get and set functions to store the data inputted by the users in the browser

--- script.js
+++ script.js
@@ -10,7 +10,27 @@ function addTask() {
         let span = document.createElement("span");
         span.innerHTML = "\u00D7";
         li.appendChild(span);
+        setStoredData();
     } else {
         alert('Please enter a task.');
     }
 }
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        setStoredData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        setStoredData();
+    }
+}, false);
+
+function setStoredData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function getShowStoredData() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+getShowStoredData();
