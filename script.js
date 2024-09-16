62effc1a4004f6fc9e9d38b8abbdf36078ddcaf6
Sothyra12 <schan256@my.centennialcollege.ca>
2024-09-15 20:21:16 -0400

    added an addTask() function and modified css and html files

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const inputBox = document.querySelector('.input-box');
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask() {
+    if (inputBox.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        inputBox.value = '';
+        let span = document.createElement("span");
+        span.innerHTML = "\u00D7";
+        li.appendChild(span);
+    } else {
+        alert('Please enter a task.');
+    }
+}
